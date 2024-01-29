0a5157e05f8c0c9cc66ed85b00286177a137c862
Paul Starodub <amorallex@gmail.com>
2024-01-29 22:42:21 +0200

    add, update urls.py

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from blog import views
+
+urlpatterns = [
+    path("", views.post_list, name="post_list"),
+    path("<int:id>/", views.post_detail, name="post_detail"),
+]
+
+app_name = "blog"
