c210b11d2cce35112018c90a50a830909dcff6fd
Paul Starodub <amorallex@gmail.com>
2024-01-29 22:38:03 +0200

    update views.py with fbv

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,17 @@
+from django.http import HttpRequest, HttpResponse, Http404
 from django.shortcuts import render
 
-# Create your views here.
+from blog.models import Post
+
+
+def post_list(request: HttpRequest) -> HttpResponse:
+    posts = Post.published.all()
+    return render(request, "blog/post/list.html", {"posts": posts})
+
+
+def post_detail(request: HttpRequest, id: int) -> HttpResponse:
+    try:
+        post = Post.published.get(id=id)
+    except Post.DoesNotExist:
+        raise Http404("No Post found.")
+    return render(request, "blog/post/detail.html", {"post": post})
