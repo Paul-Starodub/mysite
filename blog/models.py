42abfec5394b0ad3521be04b994392a7f2dd3e32
Paul Starodub <amorallex@gmail.com>
2024-01-30 10:10:30 +0200

    update models.py & list.html with get_absolute_url

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 from django.contrib.auth.models import User
 
@@ -38,3 +39,6 @@ class Post(models.Model):
 
     def __str__(self) -> str:
         return self.title
+
+    def get_absolute_url(self) -> str:
+        return reverse("blog:post_detail", args=[self.id])
