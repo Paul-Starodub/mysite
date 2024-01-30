a19ce11e6e475fa22e488ae7b234466ad27ccd81
Paul Starodub <amorallex@gmail.com>
2024-01-30 20:09:39 +0200

    update detail.html & urls.py & views.py with comment

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,9 @@ from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 from django.core.mail import send_mail
+from django.views.decorators.http import require_POST
 
-from blog.forms import EmailPostForm
+from blog.forms import EmailPostForm, CommentForm
 from blog.models import Post
 
 
@@ -25,7 +26,13 @@ def post_detail(
         publish__month=month,
         publish__day=day,
     )
-    return render(request, "blog/post/detail.html", {"post": post})
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
+    return render(
+        request,
+        "blog/post/detail.html",
+        {"post": post, "comments": comments, "form": form},
+    )
 
 
 def post_share(request: HttpRequest, post_id: int) -> HttpResponse:
@@ -46,4 +53,20 @@ def post_share(request: HttpRequest, post_id: int) -> HttpResponse:
         request,
         "blog/post/share.html",
         {"post": post, "form": form, "sent": sent},
+    )
+
+
+@require_POST
+def post_comment(request: HttpRequest, post_id: int) -> HttpResponse:
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+    return render(
+        request,
+        "blog/post/comment.html",
+        {"post": post, "form": form, "comment": comment},
     )
