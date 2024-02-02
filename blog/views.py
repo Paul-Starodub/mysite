20e84ac9a98758049898f478b89ee698d67c8e2d
Paul Starodub <amorallex@gmail.com>
2024-02-02 17:41:28 +0200

    update views.py with SearchQuery, SearchRank

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,11 @@ from django.views.generic import ListView
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from django.db.models import Count
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import (
+    SearchVector,
+    SearchQuery,
+    SearchRank,
+)
 
 from taggit.models import Tag
 
@@ -118,9 +122,17 @@ def post_search(request: HttpRequest) -> HttpResponse:
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data["query"]
-            results = Post.published.annotate(
-                search=SearchVector("title", "body"),
-            ).filter(search=query)
+            search_vector = SearchVector("title", "body")
+            search_query = SearchQuery(query)
+            results = (
+                Post.published.annotate(
+                    search=search_vector,
+                    rank=SearchRank(search_vector, search_query),
+                )
+                .filter(search=search_query)
+                .order_by("-rank")
+            )
+
     return render(
         request,
         "blog/post/search.html",
